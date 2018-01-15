6d9a07479b5675c5a3f1950b9b6dda9cbe20559c
Markus Chiarot <markus.chiarot@intersim.ch>
2018-01-15 16:25:33 +0100

    add 3, 2, 1 for fun

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,8 @@ let consoleElement = document.querySelector('.console');
 let console = new Console(consoleElement);
 
 console.log('Hello World!');
-console.log('Please wait 3 seconds ...');
+console.log('Please wait 3 seconds!');
+console.log('3...');
+console.delayLog(1000, '2...');
+console.delayLog(2000, '1...');
 console.delayLog(3000, 'This delayed message was brought to you by: ', Color.red('Me!'));
